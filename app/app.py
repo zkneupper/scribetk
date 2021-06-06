08e44e7ac223200a2aaa5a693040c9c0ce3b0f5d
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-06-06 15:43:12 -0400

    click cli

--- app/app.py
+++ app/app.py
@@ -6,16 +6,34 @@ import click
 from video_subtitler import VideoSubtitlerAppWrapper
 
 
-def run_video_subtitler_app():
+# @click.option("--debug",  default=1 prompt="Your name", help="The person to greet.")
+# def run_video_subtitler_app(filepath_video, filepath_annotation, debug):
+
+
+@click.command()
+@click.option(
+    "--filepath_video",
+    default="media/nasa_short.mp4",
+    prompt="filepath_video:",
+    help="The file path to the video you want to annotate.",
+)
+@click.option(
+    "--filepath_annotation",
+    default="media/annotation.json",
+    prompt="filepath_annotation:",
+    help="The file path to the json file containing the video annotations.",
+)
+@click.option(
+    "--port", default=None, prompt="port number: (e.g., 5000)", help="port number",
+)
+def run_video_subtitler_app(filepath_video, filepath_annotation, port):
     debug = True
-    filepath_video = "media/nasa_short.mp4"
-    filepath_annotation = "media/annotation.json"
 
     app_instance = VideoSubtitlerAppWrapper(
         filepath_video=filepath_video, filepath_annotation=filepath_annotation,
     )
 
-    app_instance.run(debug=debug)
+    app_instance.run(debug=debug, port=port)
 
 
 if __name__ == "__main__":
