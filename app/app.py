897ffe957e618cf8c505eded8ae975a5fc0d5a3e
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-06-06 14:57:54 -0400

    Define and use FlaskAppWrapper class

--- app/app.py
+++ app/app.py
@@ -17,61 +17,84 @@ from flask import (
 )
 
 
-# Define the Flask app
-app = Flask(__name__)
+class FlaskAppWrapper:
+    def __init__(
+        self,
+        filepath_video="media/nasa_short.mp4",
+        filepath_annotation="media/annotation.json",
+    ):
+
+        # Set paths to the video file and annotation file
+        self.filepath_video = pathlib.Path(filepath_video)
+        self.filepath_annotation = pathlib.Path(filepath_annotation)
+
+        # Define the Flask app
+        self.app = Flask(__name__)
+
+        @self.app.route("/")
+        def index():
+            return render_template("index.html")
+
+        @self.app.route("/video.mp4")
+        def video_new():
+            # filepath_video = "media/nasa_short.mp4"
+            # filepath_video = pathlib.Path(filepath_video)
+
+            if not self.filepath_video.exists():
+                print(f"filepath_video DOES NOT EXIST:\t({self.filepath_video})")
+
+            return send_from_directory(
+                self.filepath_video.parent, self.filepath_video.name,
+            )
+
+        @self.app.route("/annotation.json")
+        def annotation_new():
+            # filepath_annotation = "media/annotation.json"
+            # filepath_annotation = pathlib.Path(filepath_annotation)
+
+            if not self.filepath_annotation.exists():
+                print(
+                    f"filepath_annotation DOES NOT EXIST:\t({self.filepath_annotation})",
+                )
+
+            return send_from_directory(
+                self.filepath_annotation.parent, self.filepath_annotation.name,
+            )
+
+        @self.app.route("/endpoint", methods=["POST"])
+        def my_function():
+            data = request.get_json()
+
+            # Sort the list of annotation dictionaries
+            # so that they are ordered by the start time of each annotation.
+            data = sorted(data, key=lambda k: k["start"])
+
+            res = make_response(jsonify({"message": "JSON received"}), 200)
+
+            print(f"Saving file to : {self.filepath_annotation}")
+
+            with open(self.filepath_annotation, "w") as file:
+                # json.dump(data, file, indent=4)
+
+                # Have newline at end of json file
+                file.write(f"{json.dumps(data, indent=4)}\n")
+
+            return res
+
+    def run(self, debug=False):
+        self.app.run(debug=debug)
 
 
-filepath_json = "media/nasa_short.json"
-filepath_json = pathlib.Path(filepath_json)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/video.mp4")
-def video_new():
+def main():
     filepath_video = "media/nasa_short.mp4"
-    filepath_video = pathlib.Path(filepath_video)
-
-    if not filepath_video.exists():
-        print(f"filepath_video DOES NOT EXIST:\t({filepath_video})")
-
-    return send_from_directory(filepath_video.parent, filepath_video.name)
-
-
-@app.route("/annotation.json")
-def annotation_new():
     filepath_annotation = "media/annotation.json"
-    filepath_annotation = pathlib.Path(filepath_annotation)
 
-    if not filepath_annotation.exists():
-        print(f"filepath_annotation DOES NOT EXIST:\t({filepath_annotation})")
+    app_instance = FlaskAppWrapper(
+        filepath_video=filepath_video, filepath_annotation=filepath_annotation,
+    )
 
-    return send_from_directory(filepath_annotation.parent, filepath_annotation.name)
-
-
-@app.route("/endpoint", methods=["POST"])
-def my_function():
-    data = request.get_json()
-
-    # Sort the list of annotation dictionaries
-    # so that they are ordered by the start time of each annotation.
-    data = sorted(data, key=lambda k: k["start"])
-
-    res = make_response(jsonify({"message": "JSON received"}), 200)
-
-    print(f"Saving file to : {filepath_json}")
-
-    with open(filepath_json, "w") as file:
-        # json.dump(data, file, indent=4)
-
-        # Have newline at end of json file
-        file.write(f"{json.dumps(data, indent=4)}\n")
-
-    return res
+    app_instance.run(debug=True)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
