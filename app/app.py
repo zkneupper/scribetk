5cb89f45ebe4a600ea56dd2c9f1f2062402d7ee1
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-06-06 16:27:19 -0400

    remove cli prompts; dequote filepath strings

--- app/app.py
+++ app/app.py
@@ -6,32 +6,36 @@ import click
 from video_subtitler import VideoSubtitlerAppWrapper
 
 
+def dequote(string):
+    """Remove outer quotes if present"""
+    string_new = string.strip()
+    if (string_new[0] == string_new[-1]) and string_new.startswith(("'", '"')):
+        return string_new[1:-1]
+    return string_new
+
+
 @click.command()
 @click.option(
     "--filepath_video",
     default="media/nasa_short.mp4",
-    prompt="filepath_video:",
     help="The file path to the video you want to annotate.",
 )
 @click.option(
     "--filepath_annotation",
     default="media/annotation.json",
-    prompt="filepath_annotation:",
     help="The file path to the json file containing the video annotations.",
 )
 @click.option(
-    "--port", default=5000, prompt="port number", help="port number",
+    "--port", default=5000, help="port number",
 )
 @click.option("--debug", "-d", is_flag=True, help="Print more output.")
 @click.option(
-    "--debug",
-    "-d",
-    default=False,
-    is_flag=True,
-    prompt="debug:",
-    help="Run the app in DEBUG mode.",
+    "--debug", "-d", default=False, is_flag=True, help="Run the app in DEBUG mode.",
 )
 def run_video_subtitler_app(filepath_video, filepath_annotation, port, debug):
+
+    filepath_video = dequote(filepath_video)
+    filepath_annotation = dequote(filepath_annotation)
 
     app_instance = VideoSubtitlerAppWrapper(
         filepath_video=filepath_video, filepath_annotation=filepath_annotation,
